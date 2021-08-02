a49ba13751f7ec1fa96b0472df7a56812801a716
aanthonyl <wiseanthonyliao@gmail.com>
2021-08-02 15:01:49 -0400

    main framework

--- proj3.cpp
+++ proj3.cpp
@@ -9,52 +9,116 @@ using namespace std;
 int main()
 {
 
+
     ifstream ifs("yelp_academic_dataset_business.json");
     //ifstream ifs("small_test.json");
     json jBusiness;
-    
-    int count = 0;
-    if (ifs.is_open()) {
-        cout << "start in" << endl;
-        auto start = std::chrono::system_clock::now();
-        while (!ifs.eof()) {
-            //jBusiness = json::parse(line);
-            try
-            {
-                ifs >> jBusiness;
-                
-            }
-            catch (exception e) {
+    bool isMap = true;
+    int val;
+    cout << "Run functions with: " << endl;
+    cout << "1. Map\n" << "2. Minimum Heap" << endl;
+    cin >> val;
+    switch (val) {
+    case 1:
 
-            }
-
-        count++;
-        }   
-        auto end = std::chrono::system_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end - start;
-        //jBusiness = json::parse(ifs);
-        cout << "finished in " << elapsed_seconds.count() << " seconds, " << elapsed_seconds.count()/60 << " mins" << endl;
-    }
-    
-    /*
-        string line;
-        auto start = std::chrono::system_clock::now();
+        int count = 0;
         if (ifs.is_open()) {
-            while (getline(ifs, line)) {
-                jBusiness = json::parse(line);
+            cout << "start in" << endl;
+            auto start = std::chrono::system_clock::now();
+            while (!ifs.eof()) {
+                try
+                {
 
+                    ifs >> jBusiness;
+                    //insert to map here
+                }
+                catch (exception e) {
+
+                }
+                count++;
             }
+            auto end = std::chrono::system_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end - start;
+
+            cout << "finished in " << elapsed_seconds.count() << " seconds, " << elapsed_seconds.count() / 60 << " mins, count: " << count << endl;
         }
-        auto end = std::chrono::system_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end - start;
-        cout << "finished in " << elapsed_seconds.count() << " seconds, " << elapsed_seconds.count() / 60 << " mins" << endl;
-        */
-    ifs.close();
-    /*
-    for (auto it = jBusiness.begin(); it != jBusiness.end(); it++) {
-        cout << *it << endl;
-        count++;
-    }
+        break;
+    case 2:
+        isMap = false;
+        int count = 0;
+        if (ifs.is_open()) {
+            cout << "start in" << endl;
+            auto start = std::chrono::system_clock::now();
+            while (!ifs.eof()) {
+                //jBusiness = json::parse(line);
+                try
+                {
+
+                    ifs >> jBusiness;
+                    //insert to heap here
+                    //cities[jBusiness["city"]].zips[jBusiness["postal_code"]].businessRatings.insertVal(make_pair(calcRank(jBusiness["stars"], jBusiness["review_count"]), jBusiness["name"]));
+                }
+                catch (exception e) {
+
+                }
+                /*
+                city-> postal code -> indidual business
+                    map-> map -> map or something
     */
-    std::cout << "Hello World!\n" << count;
+                count++;
+            }
+            auto end = std::chrono::system_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end - start;
+            //jBusiness = json::parse(ifs);
+            cout << "finished in " << elapsed_seconds.count() << " seconds, " << elapsed_seconds.count() / 60 << " mins, count: " << count << endl;
+        }
+        break;
+
+    }
+    ifs.close();
+
+    //FUNCTIONALITY PAST INSERT
+    int input = 0;
+    string in;
+
+    while (input != 5) {
+    cout << "Select an option (1-5):\n1. Worst Business by city\n2. Business by city and star rating\n3. Worst business by ZIP code\n4. Worst business in the United States\n5. Exit" << endl;
+    cin >> in;
+    try {
+        input = stoi(in);
+    }
+    catch (exception e) {
+        cout << "Please input a valid digit." << endl;
+        continue;
+    }
+    if (!(input > 0 && input < 6)) {
+        cout << "Please input a valid digit." << endl;
+        continue;
+    }
+
+    switch (input) {
+    case 1:
+
+        break;
+    case 2:
+
+        break;
+    case 3:
+
+        break;
+    case 4:
+
+        break;
+    }
+
+    //1.
+
+
+
+}
+
+}
+
+void insertInto() {
+
 }
